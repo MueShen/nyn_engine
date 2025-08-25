b7daba155f1713afad894a698ad1757990dc073c
MueShen <mueshen.sh@gmail.com>
2025-08-25 10:13:08 +0300

    hello window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include "glad.h"
+#include "GLFW/glfw3.h"
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){	
+	glViewport(0,0,width,height);		
+}
 
 int main (){
-	printf("hi worlda!");
+	glfwInit();
+    	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window= glfwCreateWindow(800,600, "Not Yet Null Enginie 0.0.1", NULL, NULL);
+	if ( window== NULL){
+		printf("FAILED TO CREATE GLFW WINDOW \n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	//glad init
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		printf("Failed to initialize GLAD\n");
+		return -1;
+	}    
+	glViewport(0,0,800,600);
+	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();    
+	}
+	glfwTerminate();
 	return 0;
 }
+
+
+
+
+
+
